b1db3bc2203969071adec95333faa35510bb3ff5
SCJLabs <scj619@gmail.com>
2015-07-07 14:31:59 -0400

    Initial Release

---
+++ WiFi Hotspot Assistant/Form1.Designer.vb
@@ -0,0 +1,127 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.btn_start = New System.Windows.Forms.Button()
+        Me.btn_stop = New System.Windows.Forms.Button()
+        Me.txt_pass = New System.Windows.Forms.TextBox()
+        Me.txt_name = New System.Windows.Forms.TextBox()
+        Me.NotifyIcon1 = New System.Windows.Forms.NotifyIcon(Me.components)
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 28)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(78, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Hotspot Name:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 54)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(56, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Password:"
+        '
+        'btn_start
+        '
+        Me.btn_start.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.btn_start.Location = New System.Drawing.Point(12, 105)
+        Me.btn_start.Name = "btn_start"
+        Me.btn_start.Size = New System.Drawing.Size(75, 23)
+        Me.btn_start.TabIndex = 4
+        Me.btn_start.Text = "Start"
+        Me.btn_start.UseVisualStyleBackColor = True
+        '
+        'btn_stop
+        '
+        Me.btn_stop.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btn_stop.Location = New System.Drawing.Point(207, 105)
+        Me.btn_stop.Name = "btn_stop"
+        Me.btn_stop.Size = New System.Drawing.Size(75, 23)
+        Me.btn_stop.TabIndex = 5
+        Me.btn_stop.Text = "Stop"
+        Me.btn_stop.UseVisualStyleBackColor = True
+        '
+        'txt_pass
+        '
+        Me.txt_pass.DataBindings.Add(New System.Windows.Forms.Binding("Text", Global.WiFi_Hotspot_Assistant.My.MySettings.Default, "pass", True, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged))
+        Me.txt_pass.Location = New System.Drawing.Point(105, 51)
+        Me.txt_pass.Name = "txt_pass"
+        Me.txt_pass.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
+        Me.txt_pass.Size = New System.Drawing.Size(177, 20)
+        Me.txt_pass.TabIndex = 1
+        Me.txt_pass.Text = Global.WiFi_Hotspot_Assistant.My.MySettings.Default.pass
+        '
+        'txt_name
+        '
+        Me.txt_name.DataBindings.Add(New System.Windows.Forms.Binding("Text", Global.WiFi_Hotspot_Assistant.My.MySettings.Default, "name", True, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged))
+        Me.txt_name.Location = New System.Drawing.Point(105, 25)
+        Me.txt_name.Name = "txt_name"
+        Me.txt_name.Size = New System.Drawing.Size(177, 20)
+        Me.txt_name.TabIndex = 0
+        Me.txt_name.Text = Global.WiFi_Hotspot_Assistant.My.MySettings.Default.name
+        '
+        'NotifyIcon1
+        '
+        Me.NotifyIcon1.Icon = CType(resources.GetObject("NotifyIcon1.Icon"), System.Drawing.Icon)
+        Me.NotifyIcon1.Text = "NotifyIcon1"
+        Me.NotifyIcon1.Visible = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(294, 140)
+        Me.Controls.Add(Me.btn_stop)
+        Me.Controls.Add(Me.btn_start)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txt_pass)
+        Me.Controls.Add(Me.txt_name)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Wifi Hotspot Assistant"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents txt_name As System.Windows.Forms.TextBox
+    Friend WithEvents txt_pass As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents btn_start As System.Windows.Forms.Button
+    Friend WithEvents btn_stop As System.Windows.Forms.Button
+    Friend WithEvents NotifyIcon1 As System.Windows.Forms.NotifyIcon
+
+End Class
---
+++ WiFi Hotspot Assistant/Form1.vb
@@ -0,0 +1,29 @@
+﻿Public Class Form1
+
+    Private Sub btn_start_Click(sender As Object, e As EventArgs) Handles btn_start.Click
+        My.Settings.Save()
+
+        Dim name As String = My.Settings.name
+        Dim pass As String = My.Settings.pass
+
+
+        Dim pro = Process.Start("cmd", "/c netsh wlan set hostednetwork ssid=" & name & " key=" & pass & " mode=allow > log.txt")
+        pro.WaitForExit()
+        Dim result As String = System.IO.File.ReadAllText("log.txt")
+
+        If Not result.Contains("invalid") Then
+            Dim pro2 = Process.Start("cmd", "/c netsh wlan start hostednetwork > log.txt")
+            pro2.WaitForExit()
+            Dim result2 As String = System.IO.File.ReadAllText("log.txt")
+        End If
+
+        NotifyIcon1.ShowBalloonTip(1000, "Wifi Hotspot Assistant", result, ToolTipIcon.Info)
+
+    End Sub
+
+    Private Sub btn_stop_Click(sender As Object, e As EventArgs) Handles btn_stop.Click
+        Dim pro2 = Process.Start("cmd", "/c netsh wlan stop hostednetwork > log.txt")
+        Dim result As String = System.IO.File.ReadAllText("log.txt")
+        NotifyIcon1.ShowBalloonTip(1000, "Wifi Hotspot Assistant", result, ToolTipIcon.Info)
+    End Sub
+End Class
